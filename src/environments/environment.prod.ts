21f2182cbab5bda326075bb477762d6017a2499c
Yahir Trejo <yahardy58@gmail.com>
2018-10-04 20:40:30 -0500

    feat: firebase production suport

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCQ4K1yeKslhtm6HJL9KQ_6ylcmP1Z56cc',
+    authDomain: 'mi-natura-web.firebaseapp.com',
+    databaseURL: 'https://mi-natura-web.firebaseio.com',
+    projectId: 'mi-natura-web',
+    storageBucket: 'mi-natura-web.appspot.com',
+    messagingSenderId: '520331211571'
+  }
 };
